ea59c56ed8ae2cf90b0178a6e862c5f8fc035d33
Felipe <felipe.alcazar@alu.uclm.es>
2022-03-31 13:22:33 +0200

    build 0.1

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" ); 
+        int[] pobladosEjemplo={5, 0, 24, 8, 8, 3, 4, 9, 6, 20, 14, 23, 13, 8, 2, 19, 0, 13, 2, 11, 22, 6, 14, 7, 15, 21, 12, 13, 11, 19};
+        int maxPobladosEjemplo=7;
+        int pesoMaxEjemplo=98;
+
+        int [] pobladosEjemploOrdenado=Array.toDecreasingOrder(pobladosEjemplo);
+        //Array.display(pobladosEjemplo);
+
+        int totalWeight=0;
+        for (int i : pobladosEjemploOrdenado) {
+            if(pobladosEjemploOrdenado[i]+totalWeight<pesoMaxEjemplo && i<=maxPobladosEjemplo)
+            totalWeight=totalWeight+pobladosEjemploOrdenado[i];
+            System.out.println(i);
+        }
+
+        System.out.println(totalWeight);
     }
 }
