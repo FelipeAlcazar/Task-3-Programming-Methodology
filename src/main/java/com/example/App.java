145a2dd04cf0936b00b163b192ca43055b178359
ElenaMegia <elena.megia@alu.uclm.es>
2022-03-31 18:08:12 +0200

    build 6.0

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,16 +1,16 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
+
+
 public class App 
 {
     public static void main( String[] args )
     {
-        int[] pobladosEjemplo={5, 0, 24, 8, 8, 3, 4, 9, 6, 20, 14, 23, 13, 8, 2, 19, 0, 13, 2, 11, 22, 6, 14, 7, 15, 21, 12, 13, 11, 19};
-        int maxPobladosEjemplo=7;
-        int pesoMaxEjemplo=98;
+       //int[] pobladosEjemplo={5, 0, 24, 8, 8, 3, 4, 9, 6, 20, 14, 23, 13, 8, 2, 19, 0, 13, 2, 11, 22, 6, 14, 7, 15, 21, 12, 13, 11, 19};
+       //String filename= "Villages.txt"; 
+       int[] pobladosEjemplo=Array.fileArray( "Villages.txt" );
+       int maxPobladosEjemplo=7;
+       int pesoMaxEjemplo=98;
 
         int [] pobladosEjemploOrdenado=Array.toDecreasingOrder(pobladosEjemplo);
         Array.display(pobladosEjemploOrdenado);
@@ -28,4 +28,7 @@ public class App
         }
         System.out.println(totalWeight);
     }
+
+
+
 }
